1bef15bc5f7d02b258d8ac7b89f0a8f9e1ac446f
Viraj Rakholiya <virajrakholiya14@gmail.com>
2026-06-09 11:42:16 +0530

    add cors

--- apps/nestjs/src/main.ts
+++ apps/nestjs/src/main.ts
@@ -7,6 +7,7 @@ import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.enableShutdownHooks();
 
   const swaggerConfig = new DocumentBuilder()
