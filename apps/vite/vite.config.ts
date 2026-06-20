3745b011dfeea0875193d0afd99f872293430527
Viraj Rakholiya <virajrakholiya14@gmail.com>
2026-06-20 14:06:03 +0530

    feat: remove proxy from vite and nextjs application configurations

--- apps/vite/vite.config.ts
+++ apps/vite/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -13,12 +12,7 @@ export default defineConfig({
     },
   },
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+    port: 5173,
+    host: true,
   },
 })
