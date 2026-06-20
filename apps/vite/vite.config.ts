364f82302a5e05a39116b0a8e7ef967a9e402559
Viraj Rakholiya <virajrakholiya14@gmail.com>
2026-06-20 15:07:41 +0530

    undo changes

--- apps/vite/vite.config.ts
+++ apps/vite/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -12,7 +13,12 @@ export default defineConfig({
     },
   },
   server: {
-    port: 5173,
-    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 })
